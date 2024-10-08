c964fa76e99d25f4df50fb9036239d367483ca85
Altervl <62514019+Altervl@users.noreply.github.com>
2024-10-08 22:46:15 +0300

    Реализовать построение таблицы из БД
    Также сделать асинхронными функции импорта и чтения файла, а также построения таблицы в HTML

--- script.js
+++ script.js
@@ -3,31 +3,34 @@ const db = new Dexie("dataBase");
 
 // Создать таблицу
 db.version(1).stores({
-  dataBase: '++id, параметр, значение'
+  dataTable: '++id, параметр, значение'
 });
 
 // Функция чтения CSV файла
-function readCSV(file) {
-  let reader = new FileReader();
+async function readCSV(file) {
+  return new Promise((resolve, reject) => {
+    let reader = new FileReader();
 
-  // Прочесть, как текст
-  reader.readAsText(file);
+    // Прочесть файл, как текст
+    reader.readAsText(file);
 
-  // При успешной загрузке получить текст, спарсить, как csv файл
-  // и вернуть результат
-  reader.onload = function(event) {
-    console.log(reader.result);
+    // При успешной загрузке получить текст,
+    // спарсить, как csv файл, и вернуть результат
+    reader.onload = function(event) {
+      console.log(reader.result);
 
-    const text = event.target.result;
-    const data = parseCSV(text);
+      const text = event.target.result;
+      const data = parseCSV(text);
 
-    return data;
-  };
+      // Вернуть данные через promise
+      resolve(data);
+    };
 
-  // При ошибке вывести сообщение в консоль
-  reader.onerror = function() {
-    console.log(reader.error);
-  };
+    // При отклонении promise вернуть ошибку
+    reader.onerror = function() {
+      reject(new Error('Ошибка чтения файла'))
+    };
+  });
 };
 
 // Функция для парсинга CSV
@@ -61,18 +64,84 @@ function parseCSV(text) {
 };
 
 // Функция импорта csv файла
-function importCSV(file) {
-  const content = readCSV(file);
-  db.dataBase.bulkAdd(content);
+async function importCSV(file) {
+  try {
+    // Ждать чтения файла и парсинга данных
+    const content = await readCSV(file);
+
+    // Ждать добавления данных в БД
+    await db.dataTable.bulkAdd(content);
+  } catch (error) {
+    console.error("Ошибка импорта данных", error);
+  };
+};
+
+// Функция отображения данных из БД
+async function displayTable() {
+  try {
+    // Получить данные из БД
+    const dbData = await db.dataTable.toArray();
+
+    // Если таблица пуста, вывести об этом сообщение и выйти из функции
+    if (dbData.length === 0) {
+      console.log("Данных нет.");
+      return;
+    };
+
+    // Выбрать заголовки и тело таблицы
+    const tHeader = document.getElementById('theader');
+    const tBody = document.getElementById('tbody');
+
+    // Очистить содержимое
+    tHeader.textContent = '';
+    tBody.textContent = '';
+
+    // Создать заголовки таблицы из первой строки БД
+    const tHeaders = Object.keys(dbData[0]);
+
+    // Заполнить заголовки
+    tHeaders.forEach(header => {
+      const th = document.createElement('th');
+      th.textContent = header;
+      tHeader.appendChild(th);
+    });
+
+    // Заполнить ячейки:
+    // для каждой строки в БД создать строку в HTML...
+    dbData.forEach(row => {
+      const tr = document.createElement('tr');
+
+      // ...и в каждой созданной строке заполнить ячейки под соответствующим столбцом
+      tHeaders.forEach(header => {
+        const td = document.createElement('td');
+        td.textContent = row[header];
+
+        // Вставить ячейку в строку
+        tr.appendChild(td);
+      });
+
+      // Вставить созданную строку в тело таблицы
+      tBody.appendChild(tr);
+    });
+  } catch (error) {
+    console.error('Ошибка отображения данных')
+  };
 };
 
 // Получить выбранный файл и записать в базу данных
 const input = document.getElementById('input');
 const container = document.getElementById('container');
-csvFile.addEventListener('change', function(event) {
+input.addEventListener('change', function(event) {
   const file = event.target.files[0];
   if (file) {
     importCSV(file);
     console.log("imported!");
-  };  
+  };
+
+  displayTable();
 });
+
+// Построить таблицу при загрузке страницы
+window.onload = function() {
+  displayTable();
+};
